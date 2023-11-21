685029d104209bf9f556f42eb483f4274de8423b
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2023-11-21 20:27:51 +0100

    added prototyped crash_reporting

---
+++ crash_reporting.py
@@ -0,0 +1,17 @@
+import time
+from requests import Session
+
+url = "http://localhost:9999/crash_report"
+
+
+def send_crash_notification(message):
+    with Session() as s:
+        msg = {}
+        msg["timestamp"] = time.time()
+        msg["context"] = message
+        with open("logs/peracotta.log", "r") as fs:
+            msg["log"] = fs.read()
+        res = s.post(url, json=msg)
+
+        if res.status_code == 200:
+            return True
