bce90eaca46c34f9fdaa82d3c0cda16f24c474e1
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2023-11-21 20:29:04 +0100

    added TimeoutError Handling

--- crash_reporting.py
+++ crash_reporting.py
@@ -6,6 +6,11 @@ url = "http://localhost:9999/crash_report"
 
 def send_crash_notification(message):
     with Session() as s:
+        try:
+            s.get(url, timeout=2)
+        except TimeoutError:
+            return False
+
         msg = {}
         msg["timestamp"] = time.time()
         msg["context"] = message
